e249a57e807ad7020770f930381323d11d4b958b
oisbel <oisbelsimpv@gmail.com>
2018-01-31 11:00:31 -0600

    Add class report

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -10,10 +10,35 @@ class User(Base):
 	__tablename__ = 'user'
 
 	id = Column(Integer, primary_key = True)
-	name = Column(String(250), nullable = False)
+	nombre = Column(String(250), nullable = False)
 	email = Column(String(250), nullable = False)
 	grado = Column(String(250))
 	ministerio = Column(String(250))
 	responsabilidad = Column(String(250))
 	lugar = Column(String(250))
 	pastor = Column(String(250))
+
+class Report
+	__tablename__ = "report"
+
+	id = Column(Integer, primary_key = True)
+	fecha = Column(DateTime, default=datetime.datetime.utcnow)
+	avivamientos = Column(Integer)
+	hogares = Column(Integer)
+	estudios_establecidos = Column(Integer)
+	estudios_realizados = Column(Integer)
+	estudios_asistidos = Column(Integer)
+	biblias = Column(Integer)
+	mensajeros = Column(Integer)
+	porciones = Column(Integer)
+	visitas = Column(Integer)
+	ayunos = Column(Integer)
+	horas_ayunos = Column(Integer)
+	enfermos = Column(Integer)
+	sanidades = Column(Integer)
+	mensajes = Column(Integer)
+	cultos = Column(Integer)
+	devocionales = Column(Integer)
+	otros = Column(Integer)
+	user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
