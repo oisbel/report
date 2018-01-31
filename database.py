a7d3603e95f4e98751c1aef3c7e1e407aa297d71
oisbel <oisbelsimpv@gmail.com>
2018-01-31 10:09:55 -0600

    Add class user

--- database.py
+++ database.py
@@ -4,3 +4,16 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+
+class User(Base):
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(250), nullable = False)
+	email = Column(String(250), nullable = False)
+	grado = Column(String(250))
+	ministerio = Column(String(250))
+	responsabilidad = Column(String(250))
+	lugar = Column(String(250))
+	pastor = Column(String(250))
