e4ce85951d40395ae105a9cab47bd3a37fded4b8
oisbel <oisbelsimpv@gmail.com>
2018-02-13 10:27:24 -0600

    Add auth token to database class

--- database.py
+++ database.py
@@ -6,8 +6,11 @@ from sqlalchemy import create_engine
 
 # for generate password hash
 from passlib.apps import custom_app_context as pwd_context
+import random, string
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 
 
 class User(Base):
@@ -29,6 +32,24 @@ class User(Base):
 
 	def verify_password(self, password):
 		return pwd_context.verify(password, self.password_hash)
+
+	def generate_auth_token(self, expiration=3000):
+		s = Serializer(secret_key, expires_in = expiration)
+		return s.dumps({'id': self.id })
+
+	@staticmethod
+	def verify_auth_token(token):
+		s = Serializer(secret_key)
+		try:
+			data = s.loads(token)
+		except SignatureExpired:
+			#Valid Token, but expired
+			return None
+		except BadSignature:
+			#Invalid Token
+			return None
+		user_id = data['id']
+		return user_id
 
 	@property
 	def serialize(self):
